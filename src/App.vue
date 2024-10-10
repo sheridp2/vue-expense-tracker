f4fbf7369af9d7a61d49b8e0f920bf4bc69af69e
Patrick Sheridan <sheridp2@gmail.com>
2024-10-10 11:00:13 -0700

    Simple local storage expense tracker

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
+    <TransactionList
+      :transactions="transactions"
+      @transactionDeleted="handleTransactionDeleted"
+    />
+    <AddTransation @transactionSubmitted="handleTransactionSubmitted" />
+  </div>
+</template>
+
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransation from "./components/AddTransation.vue";
+
+import { useToast } from "vue-toastification";
+
+import { ref, computed, onMounted } from "vue";
+
+const toast = useToast();
+
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem("transactions"));
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+});
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+
+  saveTransactionsToLocalStrage()
+
+  toast.success("Transaction added");
+};
+
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction) => transaction.id != id
+  );
+  saveTransactionsToLocalStrage()
+  
+  toast.success("Transaction deleted");
+};
+
+const saveTransactionsToLocalStrage = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions.value));
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Expense Trakcer</h2>
+</template>
